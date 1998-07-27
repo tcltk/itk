24f02fa5e9013fed4f1916c97f2640b2651c24bd
stanton <stanton>
1998-07-27 18:44:23 +0000

    Initial revision

---
+++ mac/pkgIndex.tcl
@@ -0,0 +1,3 @@
+# Tcl package index file, version 1.0
+
+package ifneeded Itk 2.2 [list load [file join $dir itk22[info sharedlibextension]] Itk]
---
+++ win/pkgIndex.tcl
@@ -0,0 +1,3 @@
+# Tcl package index file, version 1.0
+
+package ifneeded Itk 2.2 [list load itk22.dll Itk]
