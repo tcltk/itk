1431998fd26f93ce9b1a3da94cb4287126e6197d
hershey <hershey>
1999-06-28 18:47:35 +0000

    remove version number from comments

--- unix/itkConfig.sh
+++ unix/itkConfig.sh
@@ -12,31 +12,31 @@
 # The information in this file is specific to a single platform.
 
 # Itcl's version number.
-ITCL_VERSION='3.0'
+ITCL_VERSION='3.1'
 ITCL_MAJOR_VERSION='3'
-ITCL_MINOR_VERSION='0'
+ITCL_MINOR_VERSION='1'
 
 # The name of the Itk library (may be either a .a file or a shared library):
-ITK_LIB_FILE=libitk3.0.so
+ITK_LIB_FILE=libitk3.1.so
 
 # String to pass to linker to pick up the Itk library from its
 # build directory.
-ITK_BUILD_LIB_SPEC='-L/home/redman/ws/incrTcl/itk/unix -litk3.0'
+ITK_BUILD_LIB_SPEC='-L/home/hershey/cvs/itcl3.1.0/itk/unix -litk3.1'
 
 # String to pass to linker to pick up the Itk library from its
 # installed directory.
-ITK_LIB_SPEC='-L/usr/local/lib -litk3.0'
+ITK_LIB_SPEC='-L/home/hershey/cvs/lib -litk3.1'
 
 # The name of the Itk stub library (a .a file):
-ITK_STUB_LIB_FILE=libitkstub3.0.a
+ITK_STUB_LIB_FILE=libitkstub3.1.a
 
 # String to pass to linker to pick up the Itk stub library from its
 # build directory.
-ITK_BUILD_STUB_LIB_SPEC='-L/home/redman/ws/incrTcl/itk/unix -litkstub3.0'
+ITK_BUILD_STUB_LIB_SPEC='-L/home/hershey/cvs/itcl3.1.0/itk/unix -litkstub3.1'
 
 # String to pass to linker to pick up the Itk stub library from its
 # installed directory.
-ITK_STUB_LIB_SPEC='-L/usr/local/lib -litkstub3.0'
+ITK_STUB_LIB_SPEC='-L/home/hershey/cvs/lib -litkstub3.1'
 
 # Location of the top-level source directories from which [incr Tk]
 # was built.  This is the directory that contains a README file as well
@@ -44,4 +44,4 @@ ITK_STUB_LIB_SPEC='-L/usr/local/lib -litkstub3.0'
 # compiled in a different place than the directory containing the source
 # files, this points to the location of the sources, not the location
 # where [incr Tk] was compiled.
-ITK_SRC_DIR='/home/redman/ws/incrTcl/itk'
+ITK_SRC_DIR='/home/hershey/cvs/itcl3.1.0/itk'
