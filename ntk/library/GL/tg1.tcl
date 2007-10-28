d35b6e29ce16d252300c09eb5030f44b9a5a8f44
wiede <wiede>
2007-10-28 22:51:38 +0000

    initial version

---
+++ ntk/library/GL/tg1.tcl
@@ -0,0 +1,11 @@
+lappend auto_path .
+namespace eval ::ntk {}
+package require TclGL
+
+puts stderr "VERSION![set ::ntk::gl::version]!"
+puts stderr "INF![::info vars ::ntk::gl::*]!"
+puts stderr "V2![set ::ntk::gl::patchLevel]!"
+::ntk::gl::GL glClearColor 75.0 75.0 75.0 100.0
+puts stderr "D1![::ntk::gl::GL glDefine2Str 0x2600]!"
+puts stderr "D2![::ntk::gl::GL glStr2Define GL_MODULATE]!"
+::ntk::gl::GL xx
