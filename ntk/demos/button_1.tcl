00e91c35644ed99f0eb82fccc4a3737e1aa7549b
wiede <wiede>
2007-11-25 14:12:01 +0000

    added some comments and header block

--- ntk/demos/button_1.tcl
+++ ntk/demos/button_1.tcl
@@ -1,4 +1,24 @@
+#---------------------------------------------------------------------------
+# ntkWidget button_1.tcl demo --
+#
+# This file contains a small demo for a button using the ntkWidget package
+#
+# Copyright (c) 2007 by Arnulf P. Wiedemann
+#
+# See the file "license.terms" for information on usage and redistribution of
+# this file, and for a DISCLAIMER OF ALL WARRANTIES.
+#
+# RCS: @(#) $Id: button_1.tcl,v 1.1.2.2 2007/11/25 14:12:01 wiede Exp $
+#--------------------------------------------------------------------------
+
+# optional command line parameter: rotate 
+# the value determines how much the button is rotated counter clock wise
+# negative values rotate clockwise
+# the value is in the range of 0 .. 360 (degrees)
+
 package require ntkWidget
+
+# the next proc is mostly for debugging a simulated command line interface
 
 proc eval_stdin {} {
     set cmd [gets stdin]
@@ -30,7 +50,7 @@ set wPath [ntk button .w -width 100 -height 100 -xoffset 50 -yoffset 50 \
 -text Arnulf -textcolor [list 0 0 0 255] -bd 2 -bg [list 255 255 255 255]]
 $wPath configure -command [list flipName $wPath] -rotate $rotate
 
-puts stderr "CFG![$wPath configure -command]!"
+# pack the widget using ntkWidget grid manager
 ntk grid $wPath
 
 fileevent stdin readable eval_stdin
