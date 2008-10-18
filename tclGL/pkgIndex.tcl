ac664c4cf00d2d2c0be786cd080dbc072fd8d8f8
wiede <wiede>
2008-10-18 13:33:17 +0000

    no longer needed moved to new project directory

--- tclGL/pkgIndex.tcl
+++
@@ -1,2 +0,0 @@
-if {[catch {package present Tcl }]} return
-package ifneeded TclGL 0.1  [list load [file join $dir libTclGL0.1.so] TclGL] 
--- tclGL/tools/genCSources.sh
+++
@@ -1,6 +0,0 @@
-scanHeaderFile.tcl < /usr/include/GL/gl.h > scannedGLDotH.txt
-makeDefineGroups.tcl < scannedGLDotH.txt
-makeFuncGroups.tcl < scannedGLDotH.txt
-scanHeaderFile.tcl < /usr/include/GL/glext.h > scannedGLextDotH.txt
-makeExtDefineGroups.tcl < scannedGLextDotH.txt
-makeExtFuncGroups.tcl < scannedGLextDotH.txt
